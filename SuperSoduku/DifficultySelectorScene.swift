d3d0b4e415c9d96766303243a1252fc542d600af
bear2029 <bear2029@gmail.com>
2015-01-06 03:11:03 -0800

    ui change

--- SuperSoduku/DifficultySelectorScene.swift
+++ SuperSoduku/DifficultySelectorScene.swift
@@ -4,11 +4,14 @@ import SpriteKit
 class DifficultySelectorScene: SKScene
 {
     var gameControllerDelegation: GameControllerDelegation?
+    class var systemFont:String {
+        return "Futura-Medium"
+    }
     override func didMoveToView(view: SKView)
     {
-        self.backgroundColor = SKColor(red: 0.50, green: 0.89, blue: 0.66, alpha: 1)
+        self.backgroundColor = SKColor(red: 0.20, green: 0.2, blue: 0.2, alpha: 1)
         var logoNode = SKSpriteNode(imageNamed: "logo")
-        logoNode.position = CGPointMake(110, -100)
+        logoNode.position = CGPointMake(110, -80)
         logoNode.anchorPoint = CGPointMake(0, 1)
         self.addChild(logoNode)
         createSelector()
@@ -16,9 +19,9 @@ class DifficultySelectorScene: SKScene
     func createSelector()
     {
         var i = 0
-        let yOffset = -220
-        let h = 25;
-        let padding = 10
+        let yOffset = -240
+        let h = 30;
+        let padding = 15
         for d in GameDifficulty.allValues{
             var link = GameLink(d: d)
             link.position = CGPointMake(160,CGFloat(yOffset-i*(h+padding)))
@@ -31,17 +34,27 @@ class DifficultySelectorScene: SKScene
 class GameLink:SKNode
 {
     let labelNode:SKLabelNode
+    let borderNode:SKShapeNode
     let difficulty:GameDifficulty
     var gameControllerDelegation:GameControllerDelegation?
     init(d:GameDifficulty)
     {
-        labelNode = SKLabelNode(fontNamed: "Cochin-Bold")
+        labelNode = SKLabelNode(fontNamed: DifficultySelectorScene.systemFont)
         labelNode.text = d.text
-        labelNode.fontSize = 16;
+        labelNode.fontSize = 12;
+        labelNode.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
+        let w = 90
+        let h = 30
+        let r = 4
+        
+        borderNode = SKShapeNode(path: CGPathCreateWithRoundedRect(CGRectMake(CGFloat(-1*w/2), CGFloat(-1*h/2), CGFloat(w), CGFloat(h)), CGFloat(r), CGFloat(r), nil))
+        borderNode.strokeColor = SKColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.9)
+        borderNode.fillColor = SKColor.clearColor()
         
         difficulty = d
         super.init()
         self.userInteractionEnabled = true
+        self.addChild(borderNode)
         self.addChild(labelNode)
     }
     required init?(coder aDecoder: NSCoder) {
--- SuperSoduku/GameScene.swift
+++ SuperSoduku/GameScene.swift
@@ -15,80 +15,6 @@ protocol GameSceneDelegation{
     func complete()
     func userSetNumber(i:Int)
 }
-class GridCell:SKNode
-{
-    var controllerDelegate:GridCellDelegation?
-    let numberNode:SKLabelNode,bgNode:SKShapeNode
-    let x:Int,y:Int,width:Float
-    
-    let errorFontColor:SKColor = SKColor.redColor()
-    let normalFontColor:SKColor = SKColor(red: 0.10, green: 0.10, blue: 0.10, alpha: 1)
-    let definedFontColor:SKColor = SKColor(red: 0.50, green: 0.45, blue: 0.33, alpha: 1)
-    let isFixed:Bool
-    
-    var number:Int?{
-        didSet{
-            self.numberNode.text = String(format: "%d", number!);
-        }
-    }
-    init(x:Int,y:Int,width:Float,isFixed:Bool)
-    {
-        self.bgNode = SKShapeNode(rect: CGRectMake(0, 0, CGFloat(width), CGFloat(width)))
-        self.bgNode.fillColor = SKColor.clearColor()
-        self.bgNode.position = CGPointMake(0, CGFloat(-1.0 * width));
-        
-        self.numberNode = SKLabelNode(fontNamed: "Chalkduster");
-        self.numberNode.fontSize = 16;
-        if(isFixed){
-            self.numberNode.fontColor = definedFontColor;
-        }else{
-            self.numberNode.fontColor = normalFontColor;
-        }
-        self.numberNode.position = CGPointMake(CGFloat(width/2), CGFloat(-1*width/2));
-        self.numberNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center
-        self.numberNode.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
-        self.numberNode.text = "_"
-        
-        self.x = x
-        self.y = y
-        self.width = width
-        self.isFixed = isFixed
-        super.init();
-        self.userInteractionEnabled = true;
-        self.addChild(bgNode)
-        self.addChild(numberNode)
-    }
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        self.controllerDelegate?.moveCursor(Pos(x:self.x,y:self.y));
-    }
-    func startFlashing()
-    {
-        self.stopFlashing()
-        self.runAction(SKAction.repeatActionForever(SKAction.sequence(
-            [SKAction.fadeAlphaTo(0.5, duration: 0.5),SKAction.fadeAlphaTo(1, duration: 0.2)]
-            )), withKey: "flash")
-    }
-    func stopFlashing()
-    {
-        removeActionForKey("flash");
-        self.alpha = 1
-    }
-    func error()
-    {
-        if(!self.isFixed){
-            self.numberNode.fontColor = errorFontColor;
-        }
-    }
-    func unerror()
-    {
-        if(!self.isFixed){
-            self.numberNode.fontColor = normalFontColor;
-        }
-    }
-}
 enum GameDifficulty:Int{
     case easy = 1,intermedia,hard,insane,nightmare
     static let allValues = [easy,intermedia,hard,insane,nightmare]
@@ -122,10 +48,93 @@ enum GameDifficulty:Int{
         }
     }
 }
+class GridCell:SKNode
+{
+    var controllerDelegate:GridCellDelegation?
+    let numberNode:SKLabelNode,bgNode:SKShapeNode
+    let x:Int,y:Int,width:Float
+    let fontSize:CGFloat = 13
+    
+    let errorFontColor:SKColor = SKColor.redColor()
+    let normalFontColor:SKColor = SKColor(red: 0.10, green: 0.10, blue: 0.10, alpha: 1)
+    let definedFontColor:SKColor = SKColor(red: 0.20, green: 0.65, blue: 0.23, alpha: 1)
+    let isFixed:Bool
+    
+    var number:Int?{
+        didSet{
+            self.numberNode.text = String(format: "%d", number!);
+        }
+    }
+    init(x:Int,y:Int,width:Float,isFixed:Bool)
+    {
+        self.bgNode = SKShapeNode(rect: CGRectMake(0, 0, CGFloat(width), CGFloat(width)))
+        self.bgNode.fillColor = SKColor.whiteColor()
+        self.bgNode.strokeColor = SKColor.clearColor()
+        self.bgNode.position = CGPointMake(0, CGFloat(-1.0 * width));
+        
+        self.numberNode = SKLabelNode(fontNamed: GameScene.systemFont);
+        self.numberNode.fontSize = fontSize;
+        if(isFixed){
+            self.numberNode.fontColor = definedFontColor;
+        }else{
+            self.numberNode.fontColor = normalFontColor;
+        }
+        self.numberNode.position = CGPointMake(CGFloat(width/2), CGFloat(-1*width/2));
+        self.numberNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center
+        self.numberNode.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
+        self.numberNode.text = ""
+        
+        self.x = x
+        self.y = y
+        self.width = width
+        self.isFixed = isFixed
+        super.init();
+        self.userInteractionEnabled = true;
+        self.addChild(bgNode)
+        self.addChild(numberNode)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        if(!self.isFixed){
+            self.controllerDelegate?.moveCursor(Pos(x:self.x,y:self.y));
+        }
+    }
+    func startFlashing()
+    {
+        self.stopFlashing()
+        self.runAction(SKAction.repeatActionForever(SKAction.sequence(
+            [SKAction.fadeAlphaTo(0.5, duration: 0.5),SKAction.fadeAlphaTo(1, duration: 0.2)]
+            )), withKey: "flash")
+    }
+    func stopFlashing()
+    {
+        removeActionForKey("flash");
+        self.alpha = 1
+    }
+    func error()
+    {
+        if(!self.isFixed){
+            self.numberNode.fontColor = errorFontColor;
+        }
+    }
+    func unerror()
+    {
+        if(!self.isFixed){
+            self.numberNode.fontColor = normalFontColor;
+        }
+    }
+}
+
 
 
 class GameScene: SKScene,GameSceneDelegation
 {
+    class var systemFont:String {
+        return "Futura-Medium"
+    }
+    let exitButtonFont = "GillSans-Bold";
     var difficulty:GameDifficulty = GameDifficulty.easy
     var gameControllerDelegation: GameControllerDelegation?
     var gridCellController:GridCellController?
@@ -133,7 +142,7 @@ class GameScene: SKScene,GameSceneDelegation
         makeGrid()
         makeNumberButtons()
         makeUtil()
-        self.backgroundColor = SKColor(red: 1.0, green: 0.89, blue: 0.66, alpha: 1)
+        self.backgroundColor = SKColor(red: 0.93, green: 0.93, blue: 0.90, alpha: 1)
     }
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         let touch:UITouch = touches.anyObject() as UITouch;
@@ -165,10 +174,10 @@ class GameScene: SKScene,GameSceneDelegation
     }
     func makeUtil()
     {
-        var exitButtonNode = SKLabelNode(fontNamed: "GillSans-Bold")
+        var exitButtonNode = SKLabelNode(fontNamed: exitButtonFont)
         exitButtonNode.text = "quit"
-        exitButtonNode.fontSize = 30
-        exitButtonNode.position = CGPointMake(270, -400)
+        exitButtonNode.fontSize = 18
+        exitButtonNode.position = CGPointMake(270, -1*(self.frame.height-40))
         exitButtonNode.fontColor = SKColor.blackColor()
         exitButtonNode.name = "exit-button"
         self.addChild(exitButtonNode)
@@ -176,11 +185,13 @@ class GameScene: SKScene,GameSceneDelegation
     func makeGrid()
     {
         let offsetX:Float = 10, offsetY:Float = -10;
+        /*
         let grid = SKSpriteNode(imageNamed: "grid_big")
         grid.position = CGPointMake(CGFloat(offsetX),CGFloat(offsetY))
         grid.anchorPoint = CGPoint(x:0,y:1);
         grid.size = CGSizeMake(300, 300)
         self.addChild(grid);
+        */
         self.gridCellController = GridCellController(width: 300, numberOfRow: 9, scene: self,x: offsetX,y: offsetY,d: self.difficulty);
     }
     func makeNumberButtons()
@@ -210,9 +221,14 @@ class GameScene: SKScene,GameSceneDelegation
 class CompleteEffect: SKNode
 {
     var score: Int
+    let gameScene:GameScene
     let label:SKLabelNode
     let background: SKShapeNode
     let overlay: SKShapeNode
+    let exitButtonNode: SKLabelNode
+    let titleFont = "AvenirNextCondensed-HeavyItalic"
+    let utilFont = "GillSans-Bold"
+    
     class func complete(scene:GameScene,score:Int)
     {
         let node = CompleteEffect(score: score,scene: scene);
@@ -224,40 +240,73 @@ class CompleteEffect: SKNode
     init(score:Int,scene:GameScene)
     {
         self.score = score
-        label = SKLabelNode(fontNamed: "AvenirNextCondensed-HeavyItalic")
-        background = SKShapeNode(rect: CGRectMake(0, 0, scene.frame.width, scene.frame.height));
-        overlay = SKShapeNode(rect: CGRectMake(0, 0, scene.frame.width, 100));
+        self.gameScene = scene
+        
+        label = SKLabelNode(fontNamed: titleFont)
+        background = SKShapeNode(rect: CGRectMake(0, 0, self.gameScene.frame.width, self.gameScene.frame.height));
+        overlay = SKShapeNode(rect: CGRectMake(0, 0, self.gameScene.frame.width, 100));
+        exitButtonNode = SKLabelNode(fontNamed: utilFont)
+
         super.init()
-        label.fontColor = SKColor.whiteColor()
-        label.text = "Congratulations!!\nScore:\(score)";
-        label.fontSize = 20;
-        label.alpha = 0
-        label.position = CGPointMake(CGFloat(scene.frame.width/2), CGFloat(-1*scene.frame.height/2));
-        label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center
-        label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
-        
-        
-        background.fillColor = SKColor(red: 0, green: 0, blue: 0, alpha: 0.4);
-        background.strokeColor = SKColor.clearColor()
-        background.position = CGPointMake(0, -1 * background.frame.height)
-        background.alpha = 0
-        
-        overlay.fillColor = SKColor(red: 0, green: 0, blue: 0, alpha: 1);
-        overlay.strokeColor = SKColor.clearColor()
-        overlay.position = CGPointMake(0, -1 * ((scene.frame.height + overlay.frame.height) / 2))
-        println("\(scene.frame.height)")
-        overlay.alpha = 0
-        
+        createTitle()
+        createBackground()
+        createOverlay()
+        createUtil()
+
         self.addChild(background)
         self.addChild(overlay)
         self.addChild(label)
 
     }
+    func createUtil()
+    {
+        let padding:CGFloat = 10
+        exitButtonNode.text = "quit"
+        exitButtonNode.fontSize = 15
+        exitButtonNode.fontColor = SKColor.whiteColor()
+        exitButtonNode.position = CGPointMake(
+            overlay.position.x + overlay.frame.width - padding,
+            overlay.position.y + padding
+        )
+        exitButtonNode.zPosition = 2
+        exitButtonNode.alpha = 0
+        exitButtonNode.name = "exit-button"
+        exitButtonNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Right
+        exitButtonNode.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Bottom
+        self.addChild(exitButtonNode)
+    }
+    func createOverlay()
+    {
+        overlay.fillColor = SKColor(red: 0, green: 0, blue: 0, alpha: 1);
+        overlay.strokeColor = SKColor.clearColor()
+        overlay.position = CGPointMake(0, -1 * ((gameScene.frame.height + overlay.frame.height) / 2))
+        overlay.alpha = 0
+    }
+    func createBackground()
+    {
+        background.fillColor = SKColor(red: 0, green: 0, blue: 0, alpha: 0.4);
+        background.strokeColor = SKColor.clearColor()
+        background.position = CGPointMake(0, -1 * background.frame.height)
+        background.alpha = 0
+
+    }
+    func createTitle()
+    {
+        label.fontColor = SKColor.whiteColor()
+        label.text = "Congratulations!!\nScore:\(score)";
+        label.fontSize = 20;
+        label.alpha = 0
+        label.position = CGPointMake(CGFloat(gameScene.frame.width/2), CGFloat(-1*self.gameScene.frame.height/2));
+        label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center
+        label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
+    }
     func play()
     {
         background.runAction(SKAction.fadeAlphaTo(1.0, duration: 0.3), completion: {
             self.overlay.runAction(SKAction.fadeAlphaTo(1.0, duration: 0.3))
-            self.label.runAction(SKAction.fadeAlphaTo(1.0, duration: 0.3))
+            self.label.runAction(SKAction.fadeAlphaTo(1.0, duration: 0.3),completion:{
+                self.exitButtonNode.runAction(SKAction.fadeAlphaTo(1, duration: 0.6))
+            })
         })
     }
     
@@ -270,18 +319,21 @@ class NumberButton: SKNode
     var i:Int
     let bgNode:SKShapeNode
     let numberNode:SKLabelNode
-    let width = 30.0
+    let width = 45.0
+    let fontSize:CGFloat = 18
     var gameSceneDelegation:GameSceneDelegation?
     init(buttonIndex:Int)
     {
         self.bgNode = SKShapeNode(rect: CGRectMake(0, 0, CGFloat(width), CGFloat(width)))
-        self.bgNode.fillColor = SKColor.clearColor()
-        self.bgNode.strokeColor = SKColor.blackColor()
+        self.bgNode = SKShapeNode(path: CGPathCreateWithRoundedRect(
+            CGRectMake(0, 0, CGFloat(width), CGFloat(width)), 4, 4, nil));
+        self.bgNode.strokeColor = SKColor.clearColor()
+        self.bgNode.fillColor = SKColor.whiteColor()
         self.bgNode.position = CGPointMake(0, CGFloat(-1.0 * width));
         
-        self.numberNode = SKLabelNode(fontNamed: "Chalkduster");
-        self.numberNode.fontSize = 16;
-        self.numberNode.fontColor = SKColor.blackColor()
+        self.numberNode = SKLabelNode(fontNamed: GameScene.systemFont);
+        self.numberNode.fontSize = fontSize;
+        self.numberNode.fontColor = SKColor(red: 0.3, green: 0.3, blue: 0.3, alpha: 1.0)
         
         self.numberNode.position = CGPointMake(CGFloat(width/2), CGFloat(-1*width/2));
         self.numberNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center
@@ -296,8 +348,11 @@ class NumberButton: SKNode
     }
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         self.gameSceneDelegation?.userSetNumber(self.i)
+        bgNode.runAction(SKAction.fadeAlphaTo(0.6, duration: 0.3))
     }
-
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        bgNode.runAction(SKAction.fadeAlphaTo(1, duration: 0.1))
+    }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -327,14 +382,18 @@ class GridCellController:GridCellDelegation
         self.cells = Array<Array<GridCell>>()
         self.difficulty = d
         self.gameSceneDelegation = scene
+        var padding:Float = 1
+        let xOffset = x+padding
+        let yOffset = y+padding
         for _x in 0...numberOfRow-1 {
             self.cells.append(Array(count: numberOfRow, repeatedValue: GridCell(x:0,y:0,width:cellWidth,isFixed: true)));
             for _y in 0...numberOfRow-1{
-                var gridCell:GridCell = GridCell(x: _x, y: _y, width: cellWidth, isFixed: self.difficulty.cellShouldBeFixed);
+                var gridCell:GridCell = GridCell(
+                    x: _x, y: _y, width: cellWidth - padding, isFixed: self.difficulty.cellShouldBeFixed);
                 gridCell.controllerDelegate = self;
                 var xMovement = Float(_x)*cellWidth;
                 var yMovement = Float(_y)*cellWidth;
-                gridCell.position = CGPointMake(CGFloat(x+xMovement), CGFloat(y-yMovement));
+                gridCell.position = CGPointMake(CGFloat(xOffset+xMovement), CGFloat(yOffset-yMovement));
                 scene.addChild(gridCell)
                 self.cells[_x][_y] = gridCell;
             }
